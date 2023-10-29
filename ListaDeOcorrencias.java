f4669fb9a0ac5d2e0b25859ee22adf890f0f961a
joaombonaldo <j.meier@edu.pucrs.br>
2023-10-28 22:16:08 -0300

    add o método 'get' na classe ListaDeOcorrencias

--- ListaDeOcorrencias.java
+++ ListaDeOcorrencias.java
@@ -85,7 +85,14 @@ public class ListaDeOcorrencias {
      * @throws IndexOutOfBoundsException se (index < 0 || index >= size())
      */    
     public Integer get(int index) {
-        return 0;
+        if ((index < 0) || (index >= size())) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.numeroDaPagina;
     }
  
         /**
